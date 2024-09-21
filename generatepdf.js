b01a41ce1a375b66d116313435d2fd5cbdca0830
Nath9666 <nathan.morel@efrei.net>
2024-09-21 20:34:24 +0200

    desing CV et refonte de celui si

--- generatepdf.js
+++ generatepdf.js
@@ -1,10 +1,15 @@
 const { jsPDF } = require("jspdf");
 const fs = require("fs");
 const path = require("path");
+const { exit } = require("process");
 
 // Charger le fichier WOFF2 en tant que base64
 const fontPath = path.join(__dirname, "./src/Assets/Roboto-Regular.ttf");
 const fontData = fs.readFileSync(fontPath, "base64");
+const fontPathBold = path.join(__dirname, "./src/Assets/Roboto-Bold.ttf");
+const fontDataBold = fs.readFileSync(fontPathBold, "base64");
+const fontPathItalic = path.join(__dirname, "./src/Assets/Roboto-Italic.ttf");
+const fontDataItalic = fs.readFileSync(fontPathItalic, "base64");
 
 // Charger et parser le fichier JSON
 const jsonPath = path.join(__dirname, "./generate.json");
@@ -15,8 +20,8 @@ const title = (doc, text, x, y, option) => {
   doc.text(text, x, y, option);
 };
 
-const text = (doc, text, x, y, option) => {
-  doc.setFontSize(12);
+const text = (doc, text, x, y, option, fontSize = 12) => {
+  doc.setFontSize(fontSize);
   doc.text(text, x, y, option);
 };
 
@@ -25,7 +30,12 @@ const generatePDF = () => {
 
   // Ajouter la police personnalisée
   doc.addFileToVFS("Roboto-Regular.ttf", fontData);
+  doc.addFileToVFS("Roboto-Bold.ttf", fontDataBold);
+  doc.addFileToVFS("Roboto-Italic.ttf", fontDataItalic);
   doc.addFont("Roboto-Regular.ttf", "Roboto", "normal");
+  doc.addFont("Roboto-Bold.ttf", "Roboto", "bold");
+  doc.addFont("Roboto-Italic.ttf", "Roboto", "italic");
+
   doc.setFont("Roboto");
 
   // Définir les dimensions
@@ -36,92 +46,293 @@ const generatePDF = () => {
   console.log(pageWidth, pageHeight, col1Width, col2Width);
 
   // Définir les couleurs
-  const blueColor = "#87CEFA";
+  const primary = "#87CEFA";
+  const secondary = "#86a8b3";
   const whiteColor = "#FFFFFF";
+  const blackColor = "#000000";
+  const greyColor = "#808080";
+
+  // Définir les marges
+  const pageMarginLeft = 5;
+  const pageMarginTop = 20;
+  const pageMarginBottom = 20;
+  const paragraphMargin = 20;
+  const lineMargin = 8;
+
+  //Definir les variables de liste
+  const number = jsonData["savoir-etre"].length;
+  const number2 = jsonData.langues.length;
+  const number3 = jsonData.passions.length;
+  const number4 = jsonData.sport.length;
+
+  //! Vérifier si la taille de la page est suffisante pour le contenu
+  if (
+    pageMarginTop +
+      5 * paragraphMargin +
+      (3 + number + number2 + number3 + number4) * lineMargin +
+      pageMarginBottom >
+    pageHeight
+  ) {
+    console.error("La taille de la page est trop petite pour le contenu");
+    exit();
+  }
 
   // Dessiner les rectangles de fond
-  doc.setFillColor(blueColor);
+  doc.setFillColor(primary);
   doc.rect(0, 0, col1Width, pageHeight, "F");
-
   doc.setFillColor(whiteColor);
   doc.rect(col1Width, 0, col2Width, pageHeight, "F");
 
   // Ajouter le contenu de la première colonne
   doc.setTextColor(whiteColor);
   doc.setFontSize(24);
-  doc.text(jsonData.nom, col1Width / 2, 20, { align: "center" });
+  doc.text(jsonData.nom, col1Width / 2, pageMarginTop, { align: "center" });
 
-  title(doc, "Coordonnées", col1Width / 2, 40, { align: "center" });
-  text(doc, `Téléphone: ${jsonData.telephone}`, 10, 50, { align: "left" });
-  text(doc, `Email: ${jsonData.email}`, 10, 60, { align: "left" });
+  title(doc, "Coordonnées", col1Width / 2, pageMarginTop + paragraphMargin, {
+    align: "center",
+  });
+  text(
+    doc,
+    `Téléphone: ${jsonData.telephone}`,
+    pageMarginLeft,
+    pageMarginTop + paragraphMargin + lineMargin,
+    { align: "left" }
+  );
+  text(
+    doc,
+    `Email: ${jsonData.email}`,
+    pageMarginLeft,
+    pageMarginTop + paragraphMargin + 2 * lineMargin,
+    { align: "left" }
+  );
 
-  title(doc, "Savoir-être", col1Width / 2, 80, { align: "center" });
+  title(
+    doc,
+    "Savoir-être",
+    col1Width / 2,
+    pageMarginTop + 2 * paragraphMargin + 2 * lineMargin,
+    { align: "center" }
+  );
   jsonData["savoir-etre"].forEach((item, index) => {
-    text(doc, `- ${item}`, 10, 90 + index * 10, { align: "left" });
+    text(
+      doc,
+      `- ${item}`,
+      pageMarginLeft,
+      pageMarginTop + 2 * paragraphMargin + 3 * lineMargin + index * lineMargin,
+      { align: "left" }
+    );
   });
 
-  title(doc, "Langues", col1Width / 2, 150, { align: "center" });
+  title(
+    doc,
+    "Langues",
+    col1Width / 2,
+    pageMarginTop + 3 * paragraphMargin + (3 + number - 1) * lineMargin,
+    { align: "center" }
+  );
   jsonData.langues.forEach((langue, index) => {
-    text(doc, `- ${langue.langue} (${langue.niveau})`, 10, 160 + index * 10, {
-      align: "left",
-    });
+    text(
+      doc,
+      `- ${langue.langue} (${langue.niveau})`,
+      pageMarginLeft,
+      pageMarginTop +
+        3 * paragraphMargin +
+        (3 + number) * lineMargin +
+        index * lineMargin,
+      {
+        align: "left",
+      }
+    );
   });
 
-  title(doc, "Passions", col1Width / 2, 200, { align: "center" });
+  title(
+    doc,
+    "Passions",
+    col1Width / 2,
+    pageMarginTop +
+      4 * paragraphMargin +
+      (3 + number + number2 - 1) * lineMargin,
+    { align: "center" }
+  );
   jsonData.passions.forEach((passion, index) => {
-    text(doc, `- ${passion}`, 10, 210 + index * 10, { align: "left" });
+    text(
+      doc,
+      `- ${passion}`,
+      pageMarginLeft,
+      pageMarginTop +
+        4 * paragraphMargin +
+        (3 + number + number2) * lineMargin +
+        index * lineMargin,
+      {
+        align: "left",
+      }
+    );
   });
 
-  title(doc, "Sports", col1Width / 2, 275, { align: "center" });
+  title(
+    doc,
+    "Sports",
+    col1Width / 2,
+    pageMarginTop +
+      5 * paragraphMargin +
+      (3 + number + number2 + number3 - 1) * lineMargin,
+    { align: "center" }
+  );
   jsonData.sport.forEach((sport, index) => {
-    text(doc, `- ${sport}`, 10, 285 + index * 10, { align: "left" });
+    text(
+      doc,
+      `- ${sport}`,
+      pageMarginLeft,
+      pageMarginTop +
+        5 * paragraphMargin +
+        (3 + number + number2 + number3) * lineMargin +
+        index * lineMargin,
+      {
+        align: "left",
+      }
+    );
   });
 
-  // Ajouter le contenu de la deuxième colonne
-  doc.setTextColor("black");
-  title(doc, "Expérience Professionnelle", col2Width, 20, { align: "center" });
+  //? Ajouter le contenu de la deuxième colonne
+  doc.setTextColor(primary);
+  title(
+    doc,
+    "Expérience Professionnelle",
+    col2Width / 2 + pageMarginLeft,
+    pageMarginTop + paragraphMargin / 2,
+    {
+      align: "left",
+    }
+  );
+  let espace = 0;
+  let index = 0;
   jsonData.experiences.forEach((experience, expIndex) => {
+    doc.setTextColor(secondary);
     if (experience.afficher) {
-      text(doc, `${experience.date}`, col2Width, 30 + expIndex * 50, {
-        align: "left",
-      });
-      text(doc, `${experience.intitule}`, col2Width, 50 + expIndex * 50, {
-        align: "left",
-      });
-      text(doc, `${experience.entreprise}`, col2Width, 60 + expIndex * 50, {
-        align: "left",
-      });
-      text(doc, `${experience.ville}`, col2Width, 70 + expIndex * 50, {
-        align: "left",
-      });
-      experience.description.forEach((desc, descIndex) => {
-        text(doc, `- ${desc}`, col2Width, 80 + expIndex * 50 + descIndex * 10, {
+      // Entreprise en gras
+      doc.setFont("Roboto", "bold");
+      text(
+        doc,
+        `${experience.entreprise}`,
+        col2Width / 2 + pageMarginLeft,
+        pageMarginTop +
+          (2 * paragraphMargin) / 2 +
+          (index * paragraphMargin) / 2 +
+          espace,
+        {
           align: "left",
-        });
+        }
+      );
+      console.log(index, experience.entreprise);
+      console.log(
+        pageMarginTop +
+          (2 * paragraphMargin) / 2 +
+          (index * paragraphMargin) / 2 +
+          espace
+      );
+      console.log(espace);
+
+      doc.setTextColor(blackColor);
+      // Intitulé en plus petit et en italique
+      doc.setFont("Roboto", "italic");
+      text(
+        doc,
+        `${experience.intitule}`,
+        col2Width / 2 + pageMarginLeft,
+        pageMarginTop +
+          (2 * paragraphMargin) / 2 +
+          (index * paragraphMargin) / 2 +
+          espace +
+          lineMargin,
+        {
+          align: "left",
+        },
+        10
+      );
+
+      // Date en police normale
+      doc.setFont("Roboto", "normal");
+      text(
+        doc,
+        `${experience.date}`,
+        col2Width / 2 + pageMarginLeft,
+        pageMarginTop +
+          (2 * paragraphMargin) / 2 +
+          (index * paragraphMargin) / 2 +
+          espace +
+          2 * lineMargin,
+        {
+          align: "left",
+        }
+      );
+
+      doc.setTextColor(greyColor);
+      // Description en bullet points
+      experience.description.forEach((desc, descIndex) => {
+        text(
+          doc,
+          `- ${desc}`,
+          col2Width / 2 + pageMarginLeft,
+          pageMarginTop +
+            (2 * paragraphMargin) / 2 +
+            (index * paragraphMargin) / 2 +
+            espace +
+            3 * lineMargin +
+            descIndex * lineMargin/2,
+          { align: "left" }, 10
+        );
       });
+      index += 1;
+      espace = lineMargin/1.4 * (2 + experience.description.length) + espace;
     }
   });
 
-  title(doc, "Formation", col2Width , 300, { align: "center" });
+  title(doc, "Formation", col2Width / 2 + pageMarginLeft, 300, {
+    align: "center",
+  });
   jsonData.formation.forEach((formation, formIndex) => {
     if (formation.afficher) {
-      text(doc, `${formation.date}`, col2Width , 310 + formIndex * 50, {
-        align: "left",
-      });
-      text(doc, `${formation.intitule}`, col2Width , 320 + formIndex * 50, {
-        align: "left",
-      });
-      text(doc, `${formation.ecole}`, col2Width , 330 + formIndex * 50, {
-        align: "left",
-      });
-      text(doc, `${formation.ville}`, col2Width , 340 + formIndex * 50, {
-        align: "left",
-      });
+      text(
+        doc,
+        `${formation.date}`,
+        col2Width / 2 + pageMarginLeft,
+        310 + formIndex * 50,
+        {
+          align: "left",
+        }
+      );
+      text(
+        doc,
+        `${formation.intitule}`,
+        col2Width / 2 + pageMarginLeft,
+        320 + formIndex * 50,
+        {
+          align: "left",
+        }
+      );
+      text(
+        doc,
+        `${formation.ecole}`,
+        col2Width / 2 + pageMarginLeft,
+        330 + formIndex * 50,
+        {
+          align: "left",
+        }
+      );
+      text(
+        doc,
+        `${formation.ville}`,
+        col2Width / 2 + pageMarginLeft,
+        340 + formIndex * 50,
+        {
+          align: "left",
+        }
+      );
       formation.description.forEach((desc, descIndex) => {
         text(
           doc,
           `- ${desc}`,
-          col2Width ,
+          col2Width / 2 + pageMarginLeft,
           350 + formIndex * 50 + descIndex * 11,
           { align: "left" }
         );
